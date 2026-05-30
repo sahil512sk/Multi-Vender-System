a2343881d7e8bf26d0705074b833f3e65ae16897
sahil <sahil.dhiman@theknowledgeacademy.com>
2026-05-30 17:20:36 +0530

    frontend

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 const app = express();
 import userRoute from './Route/userRoute.js';
+import cors from 'cors';
 
 app.use(express.json());
 app.use('/api/users', userRoute);
@@ -8,5 +9,6 @@ app.use('/api/users', userRoute);
 app.get('/', (req, res) => {
     res.send('kida fer');
 });
+app.use(cors({ origin: 'http://localhost:5000', credentials: true }));
 
 export default app;
